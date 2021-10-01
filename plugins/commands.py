9a8987d59884442b83f55a792ca71079eadb7b5b
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-01 15:23:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -112,18 +112,6 @@ async def start(bot, cmd):
 
 
 
-@Client.on_message(filters.command('total') & filters.user(ADMINS))
-async def total(bot, message):
-    """Show total files in database"""
-    msg = await message.reply("Processing...⏳", quote=True)
-    try:
-        total = await Media.count_documents()
-        await msg.edit(f'📁 Saved files: {total}')
-    except Exception as e:
-        logger.exception('Failed to check total files')
-        await msg.edit(f'Error: {e}')
-
-
 @Client.on_message(filters.command('logger') & filters.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
