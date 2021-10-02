b07686098ec204dae6edd94c4a7cd835b6072044
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-02 18:08:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -91,7 +91,10 @@ async def start(bot, cmd):
             )
         )
     else:
-        await cmd.reply_photo(photo=https://telegra.ph/file/7e7045c6d4e402131e305.jpg,
+        await cmd.reply_photo(photo=https://telegra.ph/file/7e7045c6d4e402131e305.jpg, reply_text=START_MSG,
+            parse_mode="Markdown",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton("🤴 ʙᴏᴛ ᴏᴡɴᴇʀ 🤴", url="https://t.me/im_odiyan"),
