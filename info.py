9f8e8f2a4d96b2623df55a668a46f5ff6e0f2cbf
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-02 15:43:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ default_start_msg = """
 Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
-START_IMG = environ['START_IMG']
+START_IMG = os.environ.get('START_IMG', "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
 OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
