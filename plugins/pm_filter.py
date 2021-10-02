845e58b8a051823bd03fad8383c8df06286fd0de
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-02 09:42:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -82,10 +82,10 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -95,15 +95,15 @@ async def filter(client, message):
             [InlineKeyboardButton(text="🍁 ɴᴇxᴛ ᴘᴀɢᴇ 🍁",callback_data=f"next_0_{keyword}")]
         )    
         buttons.append(
-            [InlineKeyboardButton(text=f"🧧 ᴘᴀɢᴇs 🧧 1/{data['total']}",callback_data="pages")]
+            [InlineKeyboardButton(text=f"🧧 ᴘᴀɢᴇ 1/{data['total']} 🧧",callback_data="pages")]
         )
         poster=None
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))uttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -146,9 +146,9 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -164,10 +164,9 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
-
+            await message.reply_text(f"<b>🍿 ᴍᴏᴠɪᴇ ɴᴀᴍᴇ : {search} ‌‌‌‌‎ ­  ­  ­  ­  ­/n🔔 ᴄʜᴀɴɴᴇʟ : MF 🎬 NEW RELEASE(https://t.me/joinchat/x6V1RmEmmGBhMjQ1) /n⚡️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : [ആറാം തമ്പുരാൻ²·⁰](https://t.me/Oru_autofilter_bot) /n/n👮‍♂️ ɴᴏᴛɪᴄᴇ : If you don't see the files of the movie you asked for look at next page  </b>", reply_markup=InlineKeyboardMarkup(buttons))
     
 def get_size(size):
     """Get size in readable format"""
