3197ad304213a0f2e258046fb3889c1d1617e8c2
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-10-04 21:18:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -99,7 +99,7 @@ async def start(bot, cmd):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Search Here", switch_inline_query_current_chat=''),
+                        InlineKeyboardButton("Search Herev", switch_inline_query_current_chat=''),
                         InlineKeyboardButton("Other Bots", url="https://t.me/subin_works/122")
                     ],
                     [
