df664615ac5ea42c3027673c694c56df2e45dce5
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-24 16:42:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -115,9 +115,9 @@ async def group(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>⊰᯽⊱┈❮<code>{search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </code>❯┈⊰᯽⊱\n{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>⊰᯽⊱┈❮{search}❯┈⊰᯽⊱\n{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_photo(photo=NOR_IMG, caption=f"<b>⊰᯽⊱┈❮<code>{search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </code>❯┈⊰᯽⊱\n{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=NOR_IMG, caption=f"<b>⊰᯽⊱┈❮{search}❯┈⊰᯽⊱\n{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -133,9 +133,9 @@ async def group(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>⊰᯽⊱┈❮<code>{search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </code>❯┈⊰᯽⊱\n{search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </code>{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>⊰᯽⊱┈❮{search}❯┈⊰᯽⊱\n{search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </code>{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_photo(photo=NOR_IMG, caption=f"<b>⊰᯽⊱┈❮<code>{search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </code>❯┈⊰᯽⊱\n{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=NOR_IMG, caption=f"<b>⊰᯽⊱┈❮{search}❯┈⊰᯽⊱\n{RESULT_MSG} </b>", reply_markup=InlineKeyboardMarkup(buttons))
     
 def get_size(size):
     """Get size in readable format"""
