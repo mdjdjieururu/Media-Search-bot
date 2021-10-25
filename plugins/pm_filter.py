1cd2c60499b6ace6d705258ba5e9cafdffadcb49
alwinkuttan123 <92996711+alwinkuttan123@users.noreply.github.com>
2021-10-25 11:02:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -96,7 +96,7 @@ async def group(client, message):
                 )
         else:
         	  await message.reply_text(
-                                        text="<b>Hello {first}, I could not find {search} the movie you asked for...\n\nGoogle,IMDB-Click on any button and find the CORRECT MOVIE NAME and enter it here but the movie will be available...If you do not receive the movie even after entering the correct name ...  @admin type movie name Inform the admin in this format...We will upload within 24 hours</b>",
+                                        text="<b>Hello {message.from_user.first_name}, I could not find {search} the movie you asked for...\n\nGoogle,IMDB-Click on any button and find the CORRECT MOVIE NAME and enter it here but the movie will be available...If you do not receive the movie even after entering the correct name ...  @admin type movie name Inform the admin in this format...We will upload within 24 hours</b>",
                                         reply_markup=InlineKeyboardMarkup(
                                             [
                                                 [
