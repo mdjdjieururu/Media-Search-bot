23cff261fd61de82a9bc881c7adcfa07df290e6f
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-25 10:17:32 +0530

    Add files via upload

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -89,14 +89,13 @@ async def group(client, message):
         files = await get_filter_results(query=search)
         if files:
             for file in files:
-                file_id = file.file_id
+                fie_id
                 filename = f"🧧{get_size(file.file_size)} ◽ {file.file_name}"
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=subinps_-_-_-_{file_id}")]
                 )
         else:
         	  await message.reply_text(
-                                        chat_id=message.chat.id,
                                         text="<b>Hello {update.from_user.mention}, I could not find {searc} the movie you asked for...\n\nGoogle,IMDB-Click on any button and find the CORRECT MOVIE NAME and enter it here but the movie will be available...If you do not receive the movie even after entering the correct name ...  @admin type movie name Inform the admin in this format...We will upload within 24 hours</b>",
                                         reply_markup=InlineKeyboardMarkup(
                                             [
