62ef7a20f4becef0787298efdfe6f1f885a77a69
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2021-10-26 12:53:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -107,10 +107,8 @@ async def group(client, message):
                          ]
                      )
               )
-
-def time_to_seconds(time):
-    stringt = str(time)
-    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+                
+        return
 
         if not btn:
             return
